61052f972ca64c70f71645faca0079d003e9e23b
udayshekar26 <budayshekar@gmail.com>
2024-05-16 16:26:28 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(
@@ -11,7 +12,12 @@ mongoose.connect(process.env.MONGO).then(
 ).catch(err => {
     console.log(err);
 });
+
+
+
+
 const app = express();
+app.use(express.json())
 
 app.listen(3000, () => {
 console.log('listening on port 30000');
@@ -23,3 +29,4 @@ console.log('listening on port 30000');
 //api test 
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
